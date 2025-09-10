c14188b1b409348f83bcda24dec3267a4a49397b
escchen <estellaschen@gmail.com>
2025-09-10 16:10:19 -0500

    Completed lab 3

---
+++ lab_3/lab3.py
@@ -0,0 +1,43 @@
+# classes
+class Shape():
+    def __init__(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, l, w):
+        self.length = l
+        self.width = w
+    def getArea(self):
+        return self.length * self.width
+    
+class Circle(Shape):
+    def __init__(self, r):
+        self.radius = r
+    def getArea(self):
+        return 3.14 * self.radius * self.radius
+    
+class Triangle(Shape):
+    def __init__(self, b, h):
+        self.base = b
+        self.height = h
+    def getArea(self):
+        return 0.5 * self.base * self.height
+    
+# read txt file
+file = open(r'shape.txt', 'r')
+lines = file.readlines()
+file.close()
+
+for line in lines:
+    components = line.split(',')
+    shape = components[0]
+
+    if shape == 'Rectangle':
+        rect = Rectangle(int(components[1]), int(components[2]))
+        print('Area of rectangle is:', rect.getArea())
+    elif shape == 'Circle':
+        circ = Circle(int(components[1]))
+        print('Area of circle is:', circ.getArea())
+    elif shape == 'Triangle':
+        tri = Triangle(int(components[1]), int(components[2]))
+        print('Area of triangle:', tri.getArea())
